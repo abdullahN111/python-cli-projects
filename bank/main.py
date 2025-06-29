6260d4806a8fbdf1778c2711139361ed04432312
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2025-06-29 18:43:09 +0500

    delete some files

--- bank/main.py
+++ bank/main.py
@@ -1,61 +1,76 @@
-class BankAccount:
+from typing import List
+import json
+
+        
+        
+        
+class BankAccount():
     def __init__(self, name: str, pin: int, balance: int) -> None:
         self.name = name
         self.pin = pin
         self.__balance = balance
-
-    def check_balance(self) -> str:
-        return f"\nYour balance is {self.__balance}"
-
+        
+        
+    def get_balance(self) -> int:
+        return self.__balance
+    
+    def show_balance(self) -> None:
+        print(f"\nYour balance is {self.get_balance()}\n")
+    
     def deposit(self, amount: int) -> str:
         if amount < 1:
-            return "\nInvalid amount"
+            return "Invalid Amount\n"
         self.__balance += amount
-        return f"\n{amount} deposited successfully. Your new balance is {self.__balance}\n"
+        return f"\n{amount} deposited successfully. Your new balance is {self.get_balance()}\n"
 
     def withdraw(self, amount: int) -> str:
-        if amount > self.__balance:
-            return f"\nInsufficient funds! Your balance is {self.__balance}\n"
+        if amount > self.get_balance():
+            return "Low Balance! Please enter valid amount.\n"
+        
         self.__balance -= amount
-        return f"\n{amount} withdrawn successfully. New balance: {self.__balance}\n"
-
-
+        return f"\n{amount} withdrawn successfully. New balance: {self.get_balance()}\n"
+    
+    
+    
 class Person(BankAccount):
     def __init__(self, name: str, pin: int, balance: int) -> None:
         super().__init__(name, pin, balance)
 
+with open("users.json", "r") as file:
+    users_data = json.load(file)
 
-users: list[Person] = [
-    Person("Abdullah", 1111, 100000),
-    Person("Rohan", 2222, 20000),
-    Person("Uzair", 3333, 80000),
-    Person("Sufyan", 4444, 60000)
+users: List[Person] = [
+  Person(user['name'], user['pin'], user['balance']) for user in users_data
 ]
+
 
 actions: list[str] = ["Exit the program", "Check Balance", "Deposit", "Withdraw"]
 
 def main() -> None:
     print("\n<---Welcome to ABD Bank--->")
-    print("Please enter your name and pin to continue.\n")
-
-    while True:
+    print("\n>>> Please enter your name and pin to continue.\n")
+    
+    matched_user = None
+    condition = True
+    while condition:
+        
         user_name: str = input("Enter your name: ")
         try:
             user_pin: int = int(input("Enter your 4 digit pin: "))
         except ValueError:
             print("\nInvalid input. Pin must be a 4-digit number.\n")
             continue
-
-        matched_user = None
+        
         for user in users:
-            if user.name == user_name and user.pin == user_pin:
+            if user_name ==  user.name and user_pin == user.pin:
                 matched_user = user
-                break
-
+                condition = False
+                
         if not matched_user:
             print("\nInvalid name or pin.\n")
             continue
-
+        
+        
         print(f"\nWelcome {matched_user.name}!\n")
 
         while True:
@@ -69,10 +84,10 @@ def main() -> None:
                 continue
 
             if choice == 0:
-                print("\nThank you for banking with us. Goodbye!\n")
+                print("\nThank you for using our ATM. Goodbye!\n")
                 return
             elif choice == 1:
-                print(matched_user.check_balance())
+                matched_user.show_balance()
             elif choice == 2:
                 try:
                     amount = int(input("\nEnter amount to deposit: "))
